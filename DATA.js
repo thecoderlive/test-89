fbfa9ce344034348a0f5ac11a1060beb3f709215
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-03 00:49:27 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "1a26970e-7dbd-4998-ba77-f7819313bc31",
+    "home_feed": [
+        {
+            "id": "1fb8fed9-24d1-45d6-9245-9d870ff56f28",
+            "artist_name": "factor",
+            "home_carousel": [
+                {
+                    "id": "248d971f-e86a-44df-9bc6-b3a250f185de",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/154.png"
+                },
+                {
+                    "id": "0a33ab53-655d-49a1-b51e-aab005bf3645",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/287.png"
+                },
+                {
+                    "id": "476561fd-641a-4528-851b-abfea9858709",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/190.png"
+                },
+                {
+                    "id": "11a28ece-35ce-45e2-9301-a3454eb25e67",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/157.png"
+                },
+                {
+                    "id": "4dea4329-9f81-4651-bb67-cbc0571b9477",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/330.png"
+                }
+            ],
+            "art_caption": "key",
+            "art_description": "Action skill ask suffer benefit establish include. City rate majority at agent office."
+        },
+        {
+            "id": "10f98272-9522-4ee7-bcf2-9ddd76e61e19",
+            "artist_name": "skin",
+            "home_carousel": [
+                {
+                    "id": "ae8de6ba-e159-4561-8e97-de17a68eee47",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/59.png"
+                },
+                {
+                    "id": "11f0d77d-f199-4dae-9438-121e98b35ca7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/310.png"
+                },
+                {
+                    "id": "b71acc62-fcff-4aa5-ab15-9ae62ff969be",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/66.png"
+                },
+                {
+                    "id": "0509e32f-106b-4b12-8ec8-2282d9c2521f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/50.png"
+                },
+                {
+                    "id": "82bb566a-9e20-4a44-8eb2-89ccab82f9bc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/138.png"
+                }
+            ],
+            "art_caption": "real",
+            "art_description": "Discover foreign audience indicate. Fill wait dream might process fill. Reality we fast responsibility rich south."
+        },
+        {
+            "id": "d1dbb776-4227-4f51-a25e-8a0a1b73359f",
+            "artist_name": "easy",
+            "home_carousel": [
+                {
+                    "id": "1383c158-39be-4618-af3e-fcb12158290a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/109.png"
+                },
+                {
+                    "id": "9e2f76ba-fe71-4b1d-b9c2-d0731edb9384",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/360.png"
+                },
+                {
+                    "id": "5fe73d41-2834-4388-b632-37ef281ba57f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/191.png"
+                },
+                {
+                    "id": "f856c32c-66a6-456d-8465-42c3d2f62b85",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/252.png"
+                },
+                {
+                    "id": "37369869-174b-4385-9119-351a600f5f23",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/265.png"
+                }
+            ],
+            "art_caption": "mention",
+            "art_description": "Something simply talk trouble use process per."
+        },
+        {
+            "id": "b7f4695b-2074-496f-a9a8-fe51885919fe",
+            "artist_name": "speak",
+            "home_carousel": [
+                {
+                    "id": "14048582-4db3-4919-84b7-5f8561d27b39",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/264.png"
+                },
+                {
+                    "id": "c3d8cca5-d458-425a-a6d7-2c69e7067e46",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/161.png"
+                },
+                {
+                    "id": "002d2598-c4f6-4ba6-96de-2557f2413c73",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/354.png"
+                },
+                {
+                    "id": "131baa9d-f657-420e-a65f-44dbde39daf7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/158.png"
+                },
+                {
+                    "id": "c6b9b090-43d4-48a4-9a5c-42e93d7119d8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/83.png"
+                }
+            ],
+            "art_caption": "management",
+            "art_description": "Number free toward. Law animal than forget writer. Girl wall environment why."
+        },
+        {
+            "id": "3c5718bf-677f-4d40-9824-657f656e1b11",
+            "artist_name": "fight",
+            "home_carousel": [
+                {
+                    "id": "d7370ac7-cf9c-4d6e-83eb-7dcce11dd158",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/74.png"
+                },
+                {
+                    "id": "a15461b3-bcb8-4264-b6f0-c01940b5a355",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/125.png"
+                },
+                {
+                    "id": "4767c0ae-bd51-4ed2-b9b6-ab97508b1f3f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/359.png"
+                },
+                {
+                    "id": "03ea0a78-bfb9-4cb1-a03f-42268798547f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/113.png"
+                },
+                {
+                    "id": "18db84a4-b2f5-4a87-a90b-7fc98a0165b8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/54.png"
+                }
+            ],
+            "art_caption": "off",
+            "art_description": "Resource point million. Issue care against cultural beautiful of."
+        }
+    ]
+}
